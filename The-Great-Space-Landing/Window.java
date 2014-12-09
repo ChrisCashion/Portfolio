9b904f543aaa31f344d81bd3fa6b05e1aa38c321
chris cashion <nicdchris@gmail.com>
2014-12-09 15:00:10 -0800

    Create Window.java

---
+++ The-Great-Space-Landing/Window.java
@@ -0,0 +1,52 @@
+
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+//creates the window for the game to happen in.
+//Written by Chris Cashion with help from GameTutorial.net
+
+public class Window extends JFrame{
+        
+    private Window()//Window class
+    {
+        // Sets the title for this frame.
+        this.setTitle("The Great Space Landing");
+        
+        // Sets size of the frame.
+        if(false) // Full screen mode, stubbed for future updates.
+        {
+            // Disables decorations for this frame.
+            this.setUndecorated(true);
+            // Puts the frame to full screen.
+            this.setExtendedState(this.MAXIMIZED_BOTH);
+        }
+        else // Window mode
+        {
+            // Size of the frame.
+            this.setSize(800, 600);
+            // Puts frame to center of the screen.
+            this.setLocationRelativeTo(null);
+            // So that frame cannot be resizable by the user.
+            this.setResizable(false);
+        }
+        
+        // Exit the application when user close frame.
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        this.setContentPane(new Framework());
+        
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args)//main
+    {
+        // Use the event dispatch thread to build the UI for thread-safety.
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Window();//starts our window, which runs the game
+            }
+        });
+    }
+}
